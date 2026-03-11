d8760d18b8573206f67a37bf133aa8c4dd9bcefd
jinchengwu5555-dev <jinchengwu5555@gmail.com>
2026-03-11 16:39:33 +0800

    Create app.py

---
+++ hjd2048/app.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import base64
+import ddddocr
+import flask
+import json
+
+server = flask.Flask(__name__)
+det = ddddocr.DdddOcr(det=False, ocr=False)
+
+@server.route('/slideMatch', methods=['POST'])
+def slide_match():
+    data = flask.request.get_json()
+    target_base64 = data.get('slider')
+    background_base64 = data.get('background')
+    target_bytes = base64.b64decode(target_base64)
+    background_bytes = base64.b64decode(background_base64)
+    res = det.slide_match(target_bytes, background_bytes)
+    return {'distance': res['target'][0] - 74}
+
+@server.errorhandler(500)
+def handle_error(e):
+    return json.dumps({"msg": "服务器内部错误"}), 500
+
+if __name__ == "__main__":
+    server.run(port=9991, host='0.0.0.0', debug=False)
